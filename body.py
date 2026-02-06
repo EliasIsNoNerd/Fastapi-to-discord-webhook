77346b6310a3c5675783eb9f9a5e718b9cac2e33
Asya <github-asya@yanderecore.cat>
2026-02-05 22:44:03 -0500

    Add documentation link in body.py
    Added a comment with a link to documentation.

--- body.py
+++ body.py
@@ -1,3 +1,7 @@
 import requests
 
 r = requests.post('http://127.0.0.1:3933/webhook-discord', params={'message': 'MESSAGE HERE'},)
+
+"""
+OR INSTEAD OF WRITING THAT YOU CAN DO IT HERE http://127.0.0.1:3933/docs
+"""
